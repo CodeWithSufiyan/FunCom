ece5a2fa3a8c5f709171f379e440298666acb49d
bazilab16 <bazilab16@gmail.com>
2020-04-26 11:10:07 +0530

    customer

---
+++ AdminPortal/Migrations/20200425164105_edit_CustomerModel_1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminPortal.Migrations
+{
+    public partial class edit_CustomerModel_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductCategory = table.Column<string>(nullable: true),
+                    Conditions_Allowed = table.Column<string>(nullable: true),
+                    Approval_Required = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
