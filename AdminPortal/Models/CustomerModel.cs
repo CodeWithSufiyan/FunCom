8fb6f6c85f3064a4d874c664223d8e02ca25c42c
bazilab16 <bazilab16@gmail.com>
2020-05-10 19:58:04 +0530

    customer

--- AdminPortal/Models/CustomerModel.cs
+++ AdminPortal/Models/CustomerModel.cs
@@ -4,12 +4,37 @@ namespace AdminPortal.Models
 {
     public class CustomerModel
     {
-        public long Id { get; set; }
-        public string Customer_name { get; set; }
-        public string Date_of_birth { get; set; }
-        public string Age { get; set; }
-        public string Gender { get; set; }
-        public string Address { get; set; }
+        public long Customer_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Class { get; set; }
+        public string Room { get; set; }
+        public string Building { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Postal_Code { get; set; }
+        public string Country { get; set; }
+        public string Phone_Number { get; set; }
+        public string Email { get; set; }
+        public string Voice_Mail { get; set; }
+        public string Password { get; set; }
+        public string Credit_Card { get; set; }
+        public string Credit_Card_Type_Id { get; set; }
+        public string Card_Exp_Month { get; set; }
+        public string Card_Exp_Year { get; set; }
+        public string Billing_Address { get; set; }
+        public string Billing_City { get; set; }
+        public string Billing_Region { get; set; }
+        public string Billing_PostalCode { get; set; }
+        public string Billing_Country { get; set; }
+        public string Shipping_Address { get; set; }
+        public string Shipping_City { get; set; }
+        public string Shipping_Region { get; set; }
+        public string Shipping_PostalCode { get; set; }
+        public string Shipping_Country { get; set; }
+        public string Date_Entered { get; set; }
 
     
     }
